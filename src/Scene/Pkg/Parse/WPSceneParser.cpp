c79db161452b0d777f30fe429952c416672597f8
hypengw <hypengwip@gmail.com>
2026-07-03 18:57:25 +0000

    scene: fix scroll effect final composite

--- src/Scene/Pkg/Parse/WPSceneParser.cpp
+++ src/Scene/Pkg/Parse/WPSceneParser.cpp
@@ -1159,7 +1159,8 @@ bool UsesEffectQuadPositionSpace(const wpscene::Material& wpmat) {
 }
 
 bool CanCompositeFinalEffectShader(std::string_view shader) {
-    return IsLayerCompositeShader(shader) || shader == "effects/transform";
+    return IsLayerCompositeShader(shader) || shader == "effects/transform" ||
+           shader == "effects/scroll";
 }
 
 void NormalizeEffectPositionCurve(SceneAnimationCurve& curve) {
